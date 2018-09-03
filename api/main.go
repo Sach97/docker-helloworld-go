097e3882df31aa2ee6e4d2abf7f6a624852134ca
Sach97 <sacha.arbonel@hotmai.fr>
2018-09-03 18:52:09 +0200

    dockerized

--- api/main.go
+++ api/main.go
@@ -8,33 +8,20 @@ import (
 	_ "github.com/lib/pq"
 )
 
-const (
-	host     = "localhost"
-	port     = 5432
-	user     = "postgres-dev"
-	password = "s3cr3tp4ssw0rd"
-	dbname   = "dev"
-)
-
 func main() {
-	host := "localhost"
-	port := "5432"
-	user := os.GetEnv("POSTGRES_USER")
-	password := os.GetEnv("POSTGRES_PASSWORD")
-	dbname := os.GetEnv("POSTGRES_DB")
-	sslmode := "disable"
 
-	// host := "localhost"
-	// port := 5432
-	// user := "postgres-dev"
-	// password := "s3cr3tp4ssw0rd"
-	// dbname := "dev"
-	// sslmode := "disable"
+	host := "192.168.3.111"
+	port := 5432
+	user := "postgres-dev"
+	password := "s3cr3tp4ssw0rd"
+	dbname := "dev"
+	sslmode := "disable"
 
 	t := "host=%s port=%d user=%s password=%s dbname=%s sslmode=%s"
 
 	connectionString := fmt.Sprintf(t, host, port, user, password, dbname, sslmode)
 
+	fmt.Println(connectionString)
 	db, err := sql.Open("postgres", connectionString)
 	if err != nil {
 		fmt.Println("Error in postgres connection: ", err)
